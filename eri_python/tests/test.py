0e9926b1e02c144dda1074bee497bb0201522087
zach.lamberty <zach.lamberty@elderresearch.com>
2018-05-31 16:25:29 -0400

    python: swapping base image from lambdastack to tf

--- eri_python/tests/test.py
+++ eri_python/tests/test.py
@@ -8,8 +8,34 @@ import nltk
 import plotly
 import psycopg2
 import requests
-import seaborn
+# don't import via cli, only via web console (tk not installed)
+#import seaborn
 import sqlalchemy
 import tables
 import tqdm
 import xlrd
+
+
+# torch testing
+import torch
+assert torch.__version__ >= '0.4.0'
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+lin = nn.Linear(5, 3)
+data = torch.randn(2, 5)
+x = torch.Tensor([[0.1755, -0.3268, -0.5069], [-0.6602, 0.2260, 0.1089]])
+assert not ((x - lin(data)).abs() > 1e-4).any()
+
+
+
+# tensorflow testing
+import tensorflow
+assert tensorflow.__version__ >= '1.7.0'
+
+hello = tensorflow.constant('hello')
+with tensorflow.Session() as sess:
+    assert sess.run(hello) == b'hello'
