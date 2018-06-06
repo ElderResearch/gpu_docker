4d99f90c7bb6ece56c1beb3de04abb6fb044fdc9
zach.lamberty <zach.lamberty@elderresearch.com>
2018-06-06 17:06:26 -0400

    python and r: initial commit

---
+++ eri_python_r/install.r
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+# packages requested by users in the #r_programming slack channel on 2018-06-04
+install.packages(c('Boruta'))
+install.packages(c('caret'))
+install.packages(c('data.table'))
+install.packages(c('dplyr'))
+install.packages(c('here'))
+install.packages(c('ggplot2'))
+install.packages(c('janitor'))
+install.packages(c('lubridate'))
+install.packages(c('pbapply'))
+install.packages(c('plyr'))
+install.packages(c('ranger'))
+install.packages(c('RJDBC'))
+install.packages(c('skimr'))
+install.packages(c('sqldf'))
+install.packages(c('tidyverse'))
+install.packages(c('xlsx'))
+install.packages(c('zoo'))
