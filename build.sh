27ab000873cee78263c739a158c753696e70a6e3
andrew.stewart <andrew.stewart@elderresearch.com>
2018-11-29 23:16:35 -0500

    Remove unnecessary image tags.

--- build.sh
+++ build.sh
@@ -29,10 +29,8 @@ cd ${BASEDIR}/eri_python_r/ \
 
 echo "rebuilding dev and prod python images"
 cd ${BASEDIR}/eri_dev/ \
-    && docker build ${NO_CACHE} -t eri_dev \
-    -t eri_nogpu_dev -t eri_prod .
+    && docker build ${NO_CACHE} -t eri_dev .
 
 echo "rebuilding dev and prod python + r image"
 cd ${BASEDIR}/eri_dev_p_r/ \
-    && docker build ${NO_CACHE} -t eri_dev_p_r \
-    -t eri_nogpu_dev_p_r -t eri_prod_p_r .
+    && docker build ${NO_CACHE} -t eri_dev_p_r .
