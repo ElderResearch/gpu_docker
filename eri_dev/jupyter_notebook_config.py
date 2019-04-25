8ab4b1b622bed426521e65e2cd5f5216996a02fd
enmyj <imyjer@gmail.com>
2019-04-24 22:49:28 -0400

    update jupyter_notebook_config.py based on launch app password changes

--- eri_dev/jupyter_notebook_config.py
+++ eri_dev/jupyter_notebook_config.py
@@ -1,6 +1,4 @@
 import os
-from IPython.lib import passwd
-from IPython.lib.security import hashlib
 
 c = c  # pylint:disable=undefined-variable
 c.NotebookApp.ip = '0.0.0.0'
@@ -9,14 +7,5 @@ c.NotebookApp.open_browser = False
 
 # sets a password if PASSWORD is set in the environment
 if 'PASSWORD' in os.environ:
-    password = os.environ['PASSWORD']
-    salt = str(os.getenv('PORT', 8888))
-    if password:
-        c.NotebookApp.password = passwd(password)
-        h = hashlib.new('sha1')
-        h.update(password.encode() + salt.encode())
-        c.NotebookApp.token = h.hexdigest()
-    else:
-        c.NotebookApp.password = ''
-        c.NotebookApp.token = ''
-    del os.environ['PASSWORD']
+    c.NotebookApp.password = os.environ['PASSWORD']
+
