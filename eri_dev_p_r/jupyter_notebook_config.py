65d561cacb26f5616877a5170e58449332537dca
semperstew <andrew.stewart@elderresearch.com>
2019-05-07 00:17:16 +0100

    add basic jlab plugins. set notebook token

--- eri_dev_p_r/jupyter_notebook_config.py
+++ eri_dev_p_r/jupyter_notebook_config.py
@@ -5,11 +5,6 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_password_change=False
-
-# sets a password if PASSWORD is set in the environment
-if 'PASSWORD' in os.environ:
-    c.NotebookApp.password = os.environ['PASSWORD']
-
-if 'JUPYTERTOKEN' in os.environ:
-    c.NotebookApp.password = os.environ['JUPYTERTOKEN']
+c.NotebookApp.password = os.getenv('PASSWORD', '')
+c.NotebookApp.token = os.getenv('PASSWORD', '')
 
