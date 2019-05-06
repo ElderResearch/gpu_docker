783eabd6999263611777607c01549608f250441c
enmyj <imyjer@gmail.com>
2019-05-05 21:30:02 -0400

    fixup

--- eri_dev_p_r/jupyter_notebook_config.py
+++ eri_dev_p_r/jupyter_notebook_config.py
@@ -4,6 +4,7 @@ c = c  # pylint:disable=undefined-variable
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
+c.NotebookApp.allow_password_change=False
 
 # sets a password if PASSWORD is set in the environment
 if 'PASSWORD' in os.environ:
