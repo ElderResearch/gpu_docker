4ac474961973ced0d6c0857b01d4e80e410aed64
enmyj <imyjer@gmail.com>
2019-05-09 22:03:39 -0400

    Set Jupytertoken to hash of linux password (#68)

--- eri_dev/jupyter_notebook_config.py
+++ eri_dev/jupyter_notebook_config.py
@@ -6,5 +6,5 @@ c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_password_change=False
 c.NotebookApp.password = os.getenv('PASSWORD', '')
-c.NotebookApp.token = os.getenv('PASSWORD','')
+c.NotebookApp.token = os.getenv('JUPYTERTOKEN','')
 
