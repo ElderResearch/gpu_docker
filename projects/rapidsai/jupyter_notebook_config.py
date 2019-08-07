5eac442e4570b6595cbb3e2d29383a08d517eb45
enmyj <imyjer@gmail.com>
2019-08-06 22:25:42 -0400

    rapidsai-eri docker image with jupyter lab

---
+++ projects/rapidsai/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+import os
+
+c = c  # pylint:disable=undefined-variable
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_password_change=False
+c.NotebookApp.password = os.getenv('PASSWORD', '')
+c.NotebookApp.token = os.getenv('JUPYTERTOKEN','')
+
---
+++ projects/rapidsai/starthack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~
+jupyter-lab --config /jupyter_notebook_config.py
